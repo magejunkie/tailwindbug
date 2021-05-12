b4415a71c7dddde96c9d8c1a561c95e020103945
magejunkie <mrayan89@gmail.com>
2021-05-12 15:05:47 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+const colors = require('./node_modules/tailwindcss/colors')
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './src/**/*.php',
+        './template-parts/**/*.php',
+        './*.php',
+        './inc/**/*.php',
+        './inc/*.php',
+        './src/**/*.js',
+    ],
+    darkMode: false, //you can set it to media(uses prefers-color-scheme) or class(better for toggling modes via a button)
+    theme: {
+        extend: {
+            fontFamily: {
+                body: ['Avenir', 'sans-serif'],
+                proxima: ['"proxima-nova"', 'sans-serif'],
+            },
+        }
+    },
+    variants: {},
+    plugins: [],
+}
